770f435f57d4128461883ebc42ce84c19f51bae3
Rinat1588 <rinat15best@gmail.com>
2023-01-16 10:53:24 +0500

    Добавьте файлы проекта.

---
+++ OtchetPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using otchets.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.DataVisualization.Charting;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace otchets
+{
+    /// <summary>
+    /// Логика взаимодействия для OtchetPage.xaml
+    /// </summary>
+    public partial class OtchetPage : Page
+    {
+        Core db = new Core();
+        public OtchetPage()
+        {
+            InitializeComponent();
+        }
+     
+            private void otchet_Click(object sender, RoutedEventArgs e)
+            {
+                var allUsers = db.context.Users.ToList().OrderBy(p => p.last_name).ToList();
+
+                var application = new Excel.Application();
+                application.SheetsInNewWorkbook = allUsers.Count();
+
+                Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+                for (int i = 0; i < allUsers.Count(); i++)
+                {
+                    int startRowIndex = 1;
+
+                    Excel.Worksheet worksheet = application.Worksheets.Item[i + 1];
+                    worksheet.Name = allUsers[i].last_name;
+
+                    worksheet.Cells[1][startRowIndex] = "Дата платежа";
+                    worksheet.Cells[2][startRowIndex] = "Название";
+                    worksheet.Cells[3][startRowIndex] = "Стоимость";
+                    worksheet.Cells[4][startRowIndex] = "Количество";
+                    worksheet.Cells[5][startRowIndex] = "Сумма";
+
+                    startRowIndex++;
+
+                    var usersCategories = allUsers[i].Payment.OrderBy(p => p.date_payment).GroupBy(p => p.Category).OrderBy(p => p.Key.name_category);
+
+                    foreach (var groupCategory in usersCategories)
+                    {
+                        Excel.Range headerRange = worksheet.Range[worksheet.Cells[1][startRowIndex], worksheet.Cells[5][startRowIndex]];
+                        headerRange.Merge();
+                        headerRange.Value = groupCategory.Key.name_category;
+                        headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                        headerRange.Font.Italic = true;
+
+                        startRowIndex++;
+
+
+                        foreach (var payment in groupCategory)
+                        {
+                            worksheet.Cells[1][startRowIndex] = payment.date_payment.Value.ToString("dd.MM.yyyy");
+                            worksheet.Cells[2][startRowIndex] = payment.name;
+                            worksheet.Cells[3][startRowIndex] = payment.price;
+                            worksheet.Cells[4][startRowIndex] = payment.count;
+                            worksheet.Cells[5][startRowIndex].Formula = $"=C{startRowIndex}*D{startRowIndex}";
+                            //worksheet.Cells[3][startRowIndex].NumberFormat =
+                            //  worksheet.Cells[3][startRowIndex].NumberFormat = "#,###.00";
+
+                            startRowIndex++;
+                        }
+
+                        Excel.Range sumRange = worksheet.Range[worksheet.Cells[1][startRowIndex], worksheet.Cells[4][startRowIndex]];
+                        sumRange.Merge();
+                        sumRange.Value = "ИТОГО:";
+                        sumRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignRight;
+
+                        worksheet.Cells[5][startRowIndex].Formula = $"=SUM(E{startRowIndex - groupCategory.Count()}:" + $"E{startRowIndex - 1})";
+                        sumRange.Font.Bold = worksheet.Cells[5][startRowIndex].Font.Bold = true;
+                        //worksheet.Cells[5][startRowIndex].NumberFromat = "#,###.00";
+
+                        startRowIndex++;
+
+                        Excel.Range rangeBorders = worksheet.Range[worksheet.Cells[1][1], worksheet.Cells[5][startRowIndex - 1]];
+                        rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle =
+                        rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle =
+                        rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle =
+                        rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle =
+                        rangeBorders.Borders[Excel.XlBordersIndex.xlInsideHorizontal].LineStyle =
+                        rangeBorders.Borders[Excel.XlBordersIndex.xlInsideVertical].LineStyle =
+                        Excel.XlLineStyle.xlContinuous;
+
+                        worksheet.Columns.AutoFit();
+
+                    }
+
+                    application.Visible = true;
+                }
+            }
+
+            private void UpdateChart(object sender, SelectionChangedEventArgs e)
+            {
+                if (ComboUsers.SelectedItem is UserControl currentUser &&
+                    ComboChartTypes.SelectedItem is SeriesChartType currentType)
+                {
+                    Series currentSeries = ChartPayments.Series.FirstOrDefault();
+                    currentSeries.ChartType = currentType;
+                    currentSeries.Points.Clear();
+
+                    var categoriesList = db.context.Category.ToList();
+                    foreach (var category in categoriesList)
+                    {
+                        currentSeries.Points.AddXY(category.name_category,
+                            db.context.Payment.ToList().Where(p => p.Users == currentUser && p.Category == category).Sum(p => p.Price * p.Num));
+                    }
+                }
+            }
+     } 
+}
+
